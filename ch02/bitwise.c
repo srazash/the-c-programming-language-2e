ff86dcfc7bfa578a1dcb996d560042ec58ae6093
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-05-10 17:50:06 +0100

    Started setbits() for exercise 2-6.

--- ch02/bitwise.c
+++ ch02/bitwise.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 
-#define INITIAL 100
+/*
+    &   bitwise AND
+    |   bitwise OR
+    ^   bitwise XOR
+    <<  left shift
+    >>  right shift
+    ~   one's complement
+*/
+
+setbits(int x, int p, int n, int y);
 
 main()
 {
-    int val1 = INITIAL;
-    val1 = val1 & 0177;
-    printf("val1 & 0177 -> %d\n", val1);
+    int n = 9; /* 00001001 */
+    printf("%d\n", n);
+    n = n & 0017;
+
+ 
+ 
+    printf("%d\n", n);
+}
+
+setbits(int x, int p, int n, int y)
+{
+    return x;
 }
