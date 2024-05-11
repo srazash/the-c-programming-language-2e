bb4520752cc75291f45f4a21b610b665e1b13de7
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-05-11 14:14:10 +0100

    Exercise 2-5 WIP

--- ch02/bitwise.c
+++ ch02/bitwise.c
@@ -9,20 +9,31 @@
     ~   one's complement
 */
 
-setbits(int x, int p, int n, int y);
+unsigned getbits(unsigned x, int p, int n);
+unsigned setbits(unsigned x, int p, int n, unsigned y);
 
 main()
 {
-    int n = 9; /* 00001001 */
-    printf("%d\n", n);
-    n = n & 0017;
+    printf("%d\n", getbits(0b10011101, 4, 2));
+    printf("%d\n", getbits(0b10011101, 4, 1));
+    printf("%d\n", getbits(0b10011101, 2, 2));
+    printf("%d\n", getbits(0b10011101, 4, 3));
 
- 
- 
-    printf("%d\n", n);
+    unsigned x = 0b11110000;
+    unsigned y = 0b00111100;
+
+    printf("%d\n", setbits(x, 7, 4, y));
 }
 
-setbits(int x, int p, int n, int y)
+unsigned getbits(unsigned x, int p, int n)
 {
-    return x;
+    return (x >> (p + 1 - n)) & ~(~0 << n);
+}
+
+unsigned setbits(unsigned x, int p, int n, unsigned y)
+{
+    unsigned newx = x >> (p + 1 - n);
+    newx = x << (p + 1 - n);
+    unsigned newy = ~(~y << n);
+    return newx & newy;
 }
