0190759aae675c1466e4ac1dffa518ad0e3ca2e1
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-05-11 14:43:29 +0100

    Exercise 2-6 annotated.

--- ch02/bitwise.c
+++ ch02/bitwise.c
@@ -22,7 +22,7 @@ main()
     unsigned x = 0b11110000;
     unsigned y = 0b00111100;
 
-    printf("%d\n", setbits(x, 7, 4, y));
+    printf("%d\n", setbits(x, 7, 3, y));
 }
 
 unsigned getbits(unsigned x, int p, int n)
@@ -32,15 +32,14 @@ unsigned getbits(unsigned x, int p, int n)
 
 unsigned setbits(unsigned x, int p, int n, unsigned y)
 {
-    unsigned mask = ~(~0 << n);
-    mask = mask << (p + 1 - n);
+    /* assuming x=11110000, p=7, n=3, y=00111100 */
+    unsigned mask = ~(~0 << n); /* mask=00000111 */
+    mask = mask << (p + 1 - n); /* mask=11100000 */
 
-    x = x & ~mask;
+    x = x & ~mask; /* x=00001000*/
 
-    y = y & ~(~0 << n);
-    y = y << (p + 1 - n);
+    y = y & ~(~0 << n); /* y=00000100*/
+    y = y << (p + 1 - n); /* y=10000000*/
 
-    x = x | y;
-    
-    return x;
+    return x | y; /* x=10010000 */
 }
