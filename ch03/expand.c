ff00baa44ce9422f90a115e39ec5ecf51e378a69
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-05-23 15:17:37 +0100

    Added support functions

--- ch03/expand.c
+++ ch03/expand.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-#include <ctype.h>
 
-#define LOWER_OFFSET
-#define UPPER_OFFSET 'a' - 'A'
+#define CASE_OFFSET 'a' - 'A'
 
 void expand(char s1[], char s2[]);
+int isAlpha(char c);
+int isUpper(char c);
+int isLower(char c);
+int isNum(char c);
+char toUpper(char c);
+char toLower(char c);
 
 main()
 {
@@ -23,9 +27,52 @@ void expand(char s1[], char s2[])
 
     for (i = j = 0; s1[i] != '\0'; i++)
     {
-        if (s1[i] >= 'a' && s1[i] <= 'z' || s1[i] >= 'A' && s1[i] <= 'Z')
+        if (isAlpha(s1[i]))
         {
             if (s1[i] >= 'A' && s1[i] <= 'Z')
+                ;
         }
     }
+}
+
+int isAlpha(char c)
+{
+    if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+        return 1;
+    return 0;
+}
+
+int isUpper(char c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return 1;
+    return 0;
+}
+
+int isLower(char c)
+{
+    if (c >= 'a' && c <= 'z')
+        return 1;
+    return 0;
+}
+
+int isNum(char c)
+{
+    if (c >= '0' && c <= '9')
+        return 1;
+    return 0;
+}
+
+char toUpper(char c)
+{
+    if (isLower(c))
+        return c - CASE_OFFSET;
+    return c;
+}
+
+char toLower(char c)
+{
+    if (isUpper(c))
+        return c + CASE_OFFSET;
+    return c;
 }
